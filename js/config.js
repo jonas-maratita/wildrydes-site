1b10c9c192280f93087aaddffc6426c3fd27759a
orangebadger <jonas.maratita@gmail.com>
2023-03-10 20:17:22 -0800

    cognito configuration

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-1_9kAOLM53x
-        userPoolClientId: '', // e.g. 2d69phvafqouqk7gehmitks6sa
-        region: '' // e.g. us-east-1
+        userPoolId: 'us-east-1_9kAOLM53x', // e.g. us-east-1_9kAOLM53x
+        userPoolClientId: '2d69phvafqouqk7gehmitks6sa', // e.g. 2d69phvafqouqk7gehmitks6sa
+        region: 'us-east-1' // e.g. us-east-1
     },
     api: {
         invokeUrl: 'https://hh29jy11de.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://hh29jy11de.execute-api.us-east-1.amazonaws.com/prod',
