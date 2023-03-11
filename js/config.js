2847759fb2b7d32313952807de2c4b8ccacaba2a
orangebadger <jonas.maratita@gmail.com>
2023-03-10 20:14:03 -0800

    first commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: '', // e.g. us-east-1_9kAOLM53x
+        userPoolClientId: '', // e.g. 2d69phvafqouqk7gehmitks6sa
+        region: '' // e.g. us-east-1
+    },
+    api: {
+        invokeUrl: 'https://hh29jy11de.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://hh29jy11de.execute-api.us-east-1.amazonaws.com/prod',
+    }
+};
